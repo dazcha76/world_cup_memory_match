f8408b547d6c477c3252fab2c40c2780324c58b6
dazcha76 <dazcha@hotmail.com>
2019-04-19 14:13:46 -0700

    finally added styling to champions page!

--- main.js
+++ main.js
@@ -198,8 +198,8 @@ $(document).ready(function(){
             $('#home').css('color', 'gold');
         } else if($('#game_area').hasClass('champions_game')){
             $("body").addClass("champions_background");
-            $(".title h1").text("World Cup Champions").css('color', 'black');
-            $('#home').css('color', 'black');
+            $(".title h1").text("World Cup Champions").css('color', '#812dff');
+            $('#home').css('color', '#812dff');
             create_results_div();
         }
 
@@ -411,10 +411,10 @@ $(document).ready(function(){
             how_to_timeout = setTimeout(remove_how_to, 6000);
         } else if($('#game_area').hasClass('champions_game')){
             card_wrapper.append(card1).append(card2).append(hand).append(checkmark);
-            $('.example h1').text("Match the World Cup to the team that won it that year!").css("color", "black")
-            $('.example').css("border", "10px solid black");
+            $('.example h1').text("Match the World Cup to the team that won it that year!").css("color", "#812dff")
+            $('.example').css("border", "10px solid #812dff");
             $('.card_wrapper').css('width', '60vw');
-            $('.fas.fa-times').css("color", "black");
+            $('.fas.fa-times').css("color", "#812dff");
             $('.card1, .card2').attr('src', 'images/champions.jpg').addClass('horizontal_example_cards');
             $('.hand').css({"animation-name": "move_hand_champions", "animation-duration": "3s"});
             example1_timeout = setTimeout(flip_example1, 2000);
